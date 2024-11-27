ff4dfa4ec431d722cf7e5137fcf8d15291279c3e
fernandoperezalba <fernandoperezalba2000@gmail.com>
2024-11-27 22:27:42 +0100

    Prueba solucion cambiar ruta

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -7,7 +7,8 @@ sys.path.insert(1, basepath)
 
 
 def load_processed_data():
-    df = pd.read_csv("data/processed/df_plots.csv")
+    csv_path = os.path.join(basepath, "data/processed/df_plots.csv")
+    df = pd.read_csv(csv_path)
     df["release_date"] = pd.to_datetime(df["release_date"], format="%Y-%m-%d")
     return df
 
